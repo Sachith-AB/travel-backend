cfca57d132b2c63a4793e3a1fd7541e17362c50f
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-06-06 11:11:18 +0530

    create new project again

---
+++ src/main/java/com/travel/travel/Models/User.java
@@ -0,0 +1,21 @@
+package com.travel.travel.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private int age;
+}
