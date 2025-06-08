320b62515c69db18e58a5b86476326c932376870
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-06-08 12:14:53 +0530

    create initial crud operations

--- src/main/java/com/travel/travel/Controller/UserController.java
+++ src/main/java/com/travel/travel/Controller/UserController.java
@@ -2,10 +2,20 @@ package com.travel.travel.Controller;
 
 import com.travel.travel.Models.User;
 import com.travel.travel.Service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,5 +29,38 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getMethodName() {
+        List<User> users = userService.getUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findUserById(id);
+        return user.map(u -> new ResponseEntity<>(u, HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUserEntity(@PathVariable Long id, @RequestBody User updatedUser) {
+        try{
+            User user = userService.updateUser(id, updatedUser);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }catch(RuntimeException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        try{    
+            String user = userService.deleteUser(id);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }catch(RuntimeException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/travel/travel/Service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/travel/travel/Service/ServiceImpl/UserServiceImpl.java
@@ -3,6 +3,10 @@ package com.travel.travel.Service.ServiceImpl;
 import com.travel.travel.Models.User;
 import com.travel.travel.Repository.UserRepository;
 import com.travel.travel.Service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +20,41 @@ public class UserServiceImpl implements UserService {
     public User createUser(User user) {
         return userRepository.save(user);
     }
+
+    @Override
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User updateUser(Long id, User updatedUser) {
+        Optional<User> optionalUser = userRepository.findById(id);
+    
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            existingUser.setName(updatedUser.getName());
+            existingUser.setAge(updatedUser.getAge());
+            return userRepository.save(existingUser);
+        } else {
+                throw new RuntimeException("User not found with id " + id);
+        }
+    }
+
+	@Override
+	public String deleteUser(Long id) {
+		Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            userRepository.delete(existingUser);
+            return "User deleted id = " + id;
+        } else {
+                throw new RuntimeException("User not found with id " + id);
+        }
+	}
 }
