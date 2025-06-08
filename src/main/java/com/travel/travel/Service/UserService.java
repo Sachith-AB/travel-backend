5865b76f3d478afece69b78ca7c7f817cb9e3fea
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-06-08 12:14:53 +0530

    create initial crud operations

--- src/main/java/com/travel/travel/Service/UserService.java
+++ src/main/java/com/travel/travel/Service/UserService.java
@@ -1,7 +1,19 @@
 package com.travel.travel.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.travel.travel.Models.User;
 
 public interface UserService {
     User createUser(User user);
-}
+
+    List<User> getUsers();
+
+    Optional<User> findUserById(Long id);
+
+    User updateUser(Long id, User updatedUser);
+
+    String deleteUser(Long id);
+
+}   
