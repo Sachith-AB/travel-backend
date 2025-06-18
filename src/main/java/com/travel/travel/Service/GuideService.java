e45ba9d1a2af1c98673fe4cd1269d35974c6ad04
Hirun4 <hirun08072002@gmail.com>
2025-06-18 09:49:37 +0530

    files remove

--- src/main/java/com/travel/travel/Service/GuideService.java
+++
@@ -1,8 +0,0 @@
-package com.travel.travel.Service;
-
-import com.travel.travel.Models.Guide;
-import com.travel.travel.Models.Hotel;
-
-public interface GuideService {
-    Guide createGuide(Guide guide);
-}
