d5523b0b284821449caca484e1d906175dc86c36
Hirun4 <hirun08072002@gmail.com>
2025-06-18 09:43:46 +0530

    htel register

--- src/main/java/com/travel/travel/Repository/HotelRepository.java
+++ src/main/java/com/travel/travel/Repository/HotelRepository.java
@@ -1,10 +1,8 @@
+// src/main/java/com/travel/travel/Repository/HotelRepository.java
 package com.travel.travel.Repository;
 
 import com.travel.travel.Models.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 }
--- src/main/java/com/travel/travel/Service/HotelService.java
+++ src/main/java/com/travel/travel/Service/HotelService.java
@@ -2,10 +2,6 @@ package com.travel.travel.Service;
 
 import com.travel.travel.Models.Hotel;
 
-import java.util.Optional;
-
 public interface HotelService {
-    Hotel createHotel(Hotel hotels);
-
-    Optional<Hotel> findById(Long id);
+    Hotel registerHotel(Hotel hotel) throws Exception;
 }
