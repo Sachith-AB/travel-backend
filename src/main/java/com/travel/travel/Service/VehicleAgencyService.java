111383affb2263fb2dcf6a0353354cb1d58d4c8b
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-06-21 20:40:16 +0530

    create get by id and get by user id functions

--- src/main/java/com/travel/travel/Service/VehicleAgencyService.java
+++ src/main/java/com/travel/travel/Service/VehicleAgencyService.java
@@ -2,6 +2,14 @@ package com.travel.travel.Service;
 
 import com.travel.travel.Models.VehicleAgency;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface VehicleAgencyService {
-    VehicleAgency registerVehicleAgency(VehicleAgency vehicleAgency) throws Exception;
+    void registerVehicleAgency(VehicleAgency vehicleAgency) throws Exception;
+
+    List<VehicleAgency> getVehicleAgencies() throws  Exception;
+
+    VehicleAgency getAgenciesByUserId(Long userId) throws Exception;
+    Optional<VehicleAgency> getAgencyById(Long id) throws Exception;
 }
