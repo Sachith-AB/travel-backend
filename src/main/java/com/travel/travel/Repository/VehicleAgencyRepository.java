d9b11e54f114cbcd17e1d965633a951bac01977d
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-06-21 20:14:45 +0530

    vehicle agency registration

---
+++ src/main/java/com/travel/travel/Repository/VehicleAgencyRepository.java
@@ -0,0 +1,7 @@
+package com.travel.travel.Repository;
+
+import com.travel.travel.Models.VehicleAgency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleAgencyRepository extends JpaRepository<VehicleAgency, Long> {
+}
