117a558e76e1f93539103e00203bece1c61dae94
jayathu sanka <shjayathusanka@gmail.com>
2025-06-24 16:26:44 +0530

    get details vehicle commit

--- src/main/java/com/travel/travel/Service/VehicleService.java
+++ src/main/java/com/travel/travel/Service/VehicleService.java
@@ -1,7 +1,12 @@
 package com.travel.travel.Service;
 
 import com.travel.travel.Models.Vehicle;
+import com.travel.travel.Models.VehicleAgency;
+
+import java.util.Optional;
 
 public interface VehicleService {
     Vehicle registerVehicle(Vehicle vehicle) throws Exception;
+
+    Vehicle getVehicleById(Long id) ;
 }
